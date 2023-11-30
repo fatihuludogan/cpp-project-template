4804ff37e0de2f0a7ea97794e8ae8f83b86bef32
Fatih Uludogan <fatihuludogan06@gmail.com>
2023-11-30 21:58:07 +0300

    fix: change some files for vcpkg and personal uses

--- tests/main.cc
+++ tests/main.cc
@@ -1,18 +1,23 @@
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
+#include <gtest/gtest.h>
 
 #include "my_lib.h"
 
-TEST_CASE("Factorials are computed", "[factorial]")
+TEST(FactorialTest, FactorialsAreComputed)
 {
-    REQUIRE(factorial(0) == 1);
-    REQUIRE(factorial(1) == 1);
-    REQUIRE(factorial(2) == 2);
-    REQUIRE(factorial(3) == 6);
-    REQUIRE(factorial(10) == 3628800);
+    EXPECT_EQ(factorial(0), 1);
+    EXPECT_EQ(factorial(1), 1);
+    EXPECT_EQ(factorial(2), 2);
+    EXPECT_EQ(factorial(3), 6);
+    EXPECT_EQ(factorial(10), 3628800);
 }
 
-TEST_CASE("Test printer function", "[print_hello_world]")
+TEST(PrinterTest, TestPrintHelloWorld)
 {
-    REQUIRE(print_hello_world() == 1);
+    EXPECT_EQ(print_hello_world(), 1);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
